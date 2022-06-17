4edde77fdcdd38192c7dc1a643f4da6671be4570
Thriam <96582906+Thriam@users.noreply.github.com>
2022-06-17 15:25:20 +0530

    Update gc.js

--- plugins/gc.js
+++ plugins/gc.js
@@ -1,11 +1,11 @@
 let media = './drips.jpg'
 let handler = async (m, { conn, command }) => conn.sendButton(m.chat, `
-*Hello 👋🏻, join the official groups to have a good time using the Bot or chatting with the family of The ZIM BOT INC*
+*Hello 👋🏻, join the official groups to have a good time using the Bot*
 
 *GROUPS:*
-*1.-* https://chat.whatsapp.com/JlomZPEgo3bLmzjGUYPfyJ
+*1.-* EVA 1
 
-*2.-* https://chat.whatsapp.com/DbXBmsydWBE1ZN3EoY0hRs
+*2.-* EVA 2
 
 `.trim(), m, media, [['ZIMBOT MENU', '.menu']], m)
 handler.command = /^linkgc|grupos$/i
